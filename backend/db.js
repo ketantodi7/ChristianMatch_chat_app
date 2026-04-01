58387f54d334f0fef9ced9fd43bffaca74bc844a
Ketan Pawan Todi <95423148+ketantodi7@users.noreply.github.com>
2026-04-01 05:46:57 +0530

    changes in package.json

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-    console.log(process.env.MONGO_URL)
     await mongoose.connect(process.env.MONGO_URL, {
     }).then(() => {
         console.log("MongoDB CONNECTED");
